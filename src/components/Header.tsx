fb46a022e7211638d92c0fea9a0246c21238294d
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-16 11:06:17 +0100

    [MAIN][DELETE] Unused file

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-// src/components/Header.js
-
-export const Header = () => {
-  return (
-    <div className="bg-gray-800 text-white h-[8vh] w-[150%] flex justify-center items-center">
-      <p>EPIC POTIONS</p>
-    </div>
-  );
-};
