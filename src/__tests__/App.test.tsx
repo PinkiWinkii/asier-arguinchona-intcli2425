9ecd71d1e5d07864a37db62da5b854da91c331dd
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-16 11:05:04 +0100

    [MAIN][UPDATE] Test for listIngredients function

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom'
 import { filterByLevelRequirement, getPotionByRarity, listIngredients, findPotionByEffect, calculateCraftingTime } from '../helpers/potionHelpers';
 import { potions } from '../__mocks__/potions'; // Mock data for testing
 
-describe('Potion Functions', () => {
+describe('Potion Filtering Functions', () => {
   test('filterByLevelRequirement should return potions within level requirement', () => {
     const result = filterByLevelRequirement(potions, 18);
     expect(result).toEqual([potions[0]]);
@@ -15,8 +15,8 @@ describe('Potion Functions', () => {
 
   test('listIngredients should return the names of the ingredients of a potion', () => {
     const result = listIngredients(potions[0]);
-    const expectedIngredients = potions[0].ingredients.map(ingredient => ingredient.name); // Map to names of ingredients
-    expect(result).toEqual(expectedIngredients);
+    const expectedStringArrayResult = ['Phoenix Feather', 'Molten Ember', 'Flameflower Extract']
+    expect(result).toEqual(expectedStringArrayResult);
   });
 
   test('findPotionByEffect should return potions with specified secondary effect', () => {
